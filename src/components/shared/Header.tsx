72041c2f8181bf6a787f48a181456e3772110296
joshuascott0521 <joshuasuarezdecimo@gmail.com>
2025-05-29 08:18:25 -0500

    fusion casi ultima

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,10 +1,14 @@
 import { useNavigate } from "react-router-dom";
-import { IoIosAddCircleOutline, IoIosNotifications } from "react-icons/io";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { IoNotificationsSharp } from "react-icons/io5";
 import Serch from "../../components/shared/Serch";
 import UserDropdownMenu from "./UserDropDownMenu";
+import { useState } from "react";
+import NotificationList from "./NotificationList";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [showNotifications, setShowNotifications] = useState(false);
 
   const handleNew = () => {
     navigate("/dashboard/nuevo-pqr");
@@ -28,8 +32,19 @@ const Header = () => {
         </button>
 
         <div className="relative">
-          <IoIosNotifications className="text-3xl text-gray-500" />
-          <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full" />
+          <button
+            onClick={() => setShowNotifications(!showNotifications)}
+            className="relative hover:opacity-80 transition-opacity"
+          >
+            <IoNotificationsSharp className="text-3xl text-gray-500" />
+            <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full" />
+          </button>
+
+          {showNotifications && (
+            <div className="absolute right-0 mt-2 w-[600px] z-50 transform ">
+              <NotificationList />
+            </div>
+          )}
         </div>
 
         <UserDropdownMenu />
